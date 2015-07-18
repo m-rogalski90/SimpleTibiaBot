5263e16d3bdd79ccf1921234a8bd0e2f51767cd9
mrogalski <m.rogalski90@gmail.com>
2015-07-18 19:59:19 +0200

    Added and modified some of the memory addresses

--- Simple Tibia Bot/addresses.h
+++ Simple Tibia Bot/addresses.h
@@ -1,23 +1,45 @@
 #pragma once
 
-#ifndef OFFSET
-#define OFFSET 0x20
-
-#define A_XOR			0x4380D0 + OFFSET
-#define A_LEVEL			0x4380E8 + OFFSET
-#define A_EXPERIENCE	0x4380D8 + OFFSET
-#define A_EXPPERHOUR	0x48D83C + OFFSET
-#define A_STAMINA		0x438134 + OFFSET
-#define A_OFFTG			0x438094 + OFFSET
+#define A_XOR			0x4380F0
+#define A_LEVEL			0x438108
+#define A_EXPERIENCE	0x4380F8
+#define A_EXPPERHOUR	0x48D85C
+#define A_STAMINA		0x438154
+#define A_OFFTG			0x4380B4
+#define A_SOUL			0x43810C
 #define A_HEALTH		0x5D5000
 #define A_HEALTH_MAX	0x5D502C
-#define A_MANA			0x438100 + OFFSET
-#define A_MANA_MAX		A_XOR + 0x04
-#define A_MAGIC_LVL		0x4380F0 + OFFSET
-#define A_MAGIC_PC		0x4380F8 + OFFSET
+#define A_MANA			0x438120
+#define A_MANA_MAX		0x4380F4
+
+#ifndef SKILLS_PERCENT
+	#define SKILLS_PERCENT
+
+	#define A_MAGIC_PC		0x438118
+	#define A_FIST_PC		0x438128
+	#define A_CLUB_PC		0x43812C
+	#define A_SWORD_PC		0x438130
+	#define A_AXE_PC		0x438134
+	#define A_DIST_PC		0x438138
+	#define A_SHIELD_PC		0x43813C
+	#define A_FISH_PC		0x438140
 #endif
 
-	/*lookAddressIdChar = dectohex("0x5D5034")
+#ifndef SKILLS_VALUES
+	#define SKILLS_VALUES
+
+	#define A_MAGIC_VAL		0x438110
+	#define A_FIST_VAL		0x5D5008
+	#define A_CLUB_VAL		0x5D500C
+	#define A_SWORD_VAL		0x5D5010
+	#define A_AXE_VAL		0x5D5014
+	#define A_DIST_VAL		0x5D5018
+	#define A_SHIELD_VAL	0x5D501C
+	#define A_FISH_VAL		0x5D5020
+#endif
+
+	/*
+	lookAddressIdChar = dectohex("0x5D5034")
 		lookAddressPrimeiroIdBattle = dectohex("0x62C040") - dectohex("0xE0")
 
 		AddressSkullModeNew = dectohex("0x3D889C");
@@ -28,13 +50,11 @@
 		AddressHWND = dectohex("0x43802C") + dectohex("0x20") + dectohex("0x40")
 		AddressStatus = dectohex("0x43808C") + dectohex("0x20")
 		AddressTargetRed = dectohex("0x4380FC") + dectohex("0x20")
-		AddressFirstPc = dectohex("0x438108") + dectohex("0x20")
 		AddressWhiteTarget = dectohex("0x438118") + dectohex("0x2C")
 		lookAddressLogged = dectohex("0x438680") + dectohex("0x20")
 		AddressOfBpPointer = dectohex("0x4386CC") + dectohex("0x20")
 		AddressOfWindow = dectohex("0x4386C8") + dectohex("0x20")
 		AdressEquip = dectohex("0x4386DC") + dectohex("0x20")
-		AddressSoul = dectohex("0x4380EC") + dectohex("0x20")
 		AddressReceivBuffer = dectohex("0x438308") + dectohex("0x20")
 
 
@@ -64,13 +84,7 @@
 		AddressMouseY2 = AddressMouseX2 + 4;
 	address_item_to_be_moved = dectohex("0x5D1994") + dectohex("0x28")
 
-
-		AddressAcc = dectohex("0x5D1380")--ignore deprecated
-		AddressPass = dectohex("0x5D138C")--ignore deprecated
-		AddressIndexChar = dectohex("0x5D13BC")--ignore deprecated
-
 		AddressZGO = dectohex("0x5D5004")
-		AddressFirst = dectohex("0x5D5008")
 		AddressCap = dectohex("0x5D5024")
 
 		AddressYGO = dectohex("0x5D5028")
@@ -94,12 +108,7 @@
 
 
 
-	AddressClubPc = AddressFirstPc + 4;
-	AddressSwordPc = AddressFirstPc + 4 * 2;
-	AddressAxePc = AddressFirstPc + 4 * 3;
-	AddressDistancePc = AddressFirstPc + 4 * 4;
-	AddressShieldingPc = AddressFirstPc + 4 * 5;
-	AddressFishingPc = AddressFirstPc + 4 * 6;
+	
 
 	AddressMouse_fix_x = AddressMouseX1;
 	AddressMouse_fix_y = AddressMouse_fix_x + 4;
